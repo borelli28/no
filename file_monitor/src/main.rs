cc361f960cd17c71e9ab5c0f5015425485db3da8
Armando <borelliarmando28@gmail.com>
2024-01-02 04:12:54 -0500

    Clean up in cli_menu()

--- file_monitor/src/main.rs
+++ file_monitor/src/main.rs
@@ -84,21 +84,17 @@ fn cli_menu() {
         println!("[G] Generate Hash, [Q] Quit");
 
         let mut input = String::new();
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read line");
-    
-        let input = input.trim().to_lowercase();
-        let input: &str = input.as_str();
+        io::stdin().read_line(&mut input).expect("Failed to read line");
+        let input: String = input.trim().to_lowercase();
     
         if input == "g" {
             let hash_this_file = "./test.txt";
             let result = hash_file(hash_this_file);
-            println!("{}", result);
+            println!("{} \n", result);
         } else if input == "q" {
             break
         } else {
-            println!("nothing else")
+            println!("Invalid input \n")
         }
     }
 }
