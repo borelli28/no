56448e4d9aba741839f026cccf91607ce05968ad
Armando <borelliarmando28@gmail.com>
2024-01-07 05:52:40 -0500

    Fixed bug where non existing dirs were stopping monitor_mode from running

--- file_monitor/src/main.rs
+++ file_monitor/src/main.rs
@@ -93,26 +93,31 @@ fn monitor_mode(file_path: &str) -> Result<String, io::Error> {
             let reader = BufReader::new(file);
             for line in reader.lines() {
                 let line = line?;
-
-                for entry in fs::read_dir(line)? {
-                    let entry = entry?;
-                    let path = entry.path();
-                    if path.is_dir() {
-                        println!("path is dir");
-                    } else {
-                        let path = format!("{}", path.to_string_lossy()); // Convert PathBuff to str
-                        let hash = hash_file(&path);
-                        let hash_str: &str = &hash;
-                        let now = Utc::now();
-                        let timestamp: &str = &now.format("%Y-%m-%d %H:%M:%S").to_string();
-                        // If the line exists in hashes.json, delete it, then call write_hash()
-                        // else, call write_hash()
-                        match write_hash(hash_str, &path, timestamp) {
-                            Ok(_) => {
-                                println!("Ok");
-                            }
-                            Err(err) => {
-                                eprintln!("Error: {}", err);
+                println!("96");
+                if let Ok(entries) = fs::read_dir(&line) {
+                    for entry in entries {
+                        println!("98");
+                        let entry = entry?;
+                        let path = entry.path();
+                        println!("101");
+                        if path.is_dir() {
+                            println!("path is dir");
+                        } else {
+                            let path = format!("{}", path.to_string_lossy()); // Convert PathBuff to str
+                            println!("path: {}", path);
+                            let hash = hash_file(&path);
+                            let hash_str: &str = &hash;
+                            let now = Utc::now();
+                            let timestamp: &str = &now.format("%Y-%m-%d %H:%M:%S").to_string();
+                            // If the line exists in hashes.json, delete it, then call write_hash()
+                            // else, call write_hash()
+                            match write_hash(hash_str, &path, timestamp) {
+                                Ok(_) => {
+                                    println!("Ok");
+                                }
+                                Err(err) => {
+                                    eprintln!("Error: {}", err);
+                                }
                             }
                         }
                     }
